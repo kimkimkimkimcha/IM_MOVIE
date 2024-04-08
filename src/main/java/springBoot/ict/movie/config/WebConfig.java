057f80be583df0ceaa4aa90dc87eb30c233c84f5
C.H Kim <ckdgo9409@gmail.com>
2024-04-08 12:26:00 +0900

    b

--- src/main/java/springBoot/ict/movie/config/WebConfig.java
+++ src/main/java/springBoot/ict/movie/config/WebConfig.java
@@ -29,8 +29,8 @@ public class WebConfig {
 		
 		System.out.println("<<< WebConfig-1 >>>");
 		config.setAllowCredentials(true);
+		config.addAllowedOrigin("http://3.36.236.149:3000");
 		config.addAllowedOrigin("http://localhost:3000");
-		// config.addAllowedOrigin("http://3.36.236.149:3000");
 		config.setAllowedHeaders(Arrays.asList(
 					HttpHeaders.AUTHORIZATION,
 					HttpHeaders.CONTENT_TYPE,
